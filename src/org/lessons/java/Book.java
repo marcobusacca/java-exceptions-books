a219a6192f482b968fa067700a37246768069de9
marcobusacca <marcobusacca01@gmail.com>
2023-11-15 14:13:24 +0100

    [FEAT] class "Book" completed

--- src/org/lessons/java/Book.java
+++ src/org/lessons/java/Book.java
@@ -2,4 +2,56 @@ package org.lessons.java;
 
 public class Book {
 
+	private String titolo;
+	private int numeroPagine;
+	private String autore;
+	private String editore;
+	
+	
+	public Book(String titolo, int numeroPagine, String autore, String editore) {
+
+		setTitolo(titolo);
+		setNumeroPagine(numeroPagine);
+		setAutore(autore);
+		setEditore(editore);
+	}
+	
+	
+	public String getTitolo() {
+		return titolo;
+	}
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	public int getNumeroPagine() {
+		return numeroPagine;
+	}
+	public void setNumeroPagine(int numeroPagine) {
+		this.numeroPagine = numeroPagine;
+	}
+	public String getAutore() {
+		return autore;
+	}
+	public void setAutore(String autore) {
+		this.autore = autore;
+	}
+	public String getEditore() {
+		return editore;
+	}
+	public void setEditore(String editore) {
+		this.editore = editore;
+	}
+	
+	
+	@Override
+	public String toString() {
+		
+		return "------------------------\n"
+				+ "Libro\n"
+				+ "\nTitolo: " + getTitolo() + "\n"
+				+ "Numero pagine: " + getNumeroPagine() + "\n"
+				+ "Autore: " + getAutore() + "\n"
+				+ "Editore: " + getEditore()
+				+ "\n------------------------\n";
+	}
 }
--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+//		Book b1 = new Book("Titolo1", 30, "Autore1", "Editore1");
+//		System.out.println(b1);
+//	
+//		Book b2 = new Book("Titolo2", 23, "Autore2", "Editore2");
+//		System.out.println(b2);
 	}
 }
